ae89129e83df3b2a01d9617ec3c1a84273bf61b2
funkyV <minea.vlad@gmail.com>
2016-01-26 15:40:27 +0200

    am adaugat un buton care schimba culoarea backgroundului

--- xcodeServerTest/xcodeServerTest/ViewController.m
+++ xcodeServerTest/xcodeServerTest/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    IBOutlet UIButton *_button;
+}
 @end
 
 @implementation ViewController
@@ -17,11 +19,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view setBackgroundColor:[UIColor greenColor]];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)didPressButton:(id)sender
+{
+    UIColor *color = [UIColor whiteColor];
+    
+    if ([[self.view backgroundColor] isEqual:[UIColor whiteColor]])
+        color = [UIColor greenColor];
+    
+    [self.view setBackgroundColor: color];
 }
 
 @end
