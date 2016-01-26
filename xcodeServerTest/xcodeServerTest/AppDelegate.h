0db27cf7d82981091d6ad04818aceee0de221ab3
funkyV <minea.vlad@gmail.com>
2016-01-26 13:51:21 +0200

    am adaugat si proiectul

---
+++ xcodeServerTest/xcodeServerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xcodeServerTest
+//
+//  Created by Vlad Minea on 25/01/16.
+//  Copyright © 2016 MobileTouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xcodeServerTest/xcodeServerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xcodeServerTest
+//
+//  Created by Vlad Minea on 25/01/16.
+//  Copyright © 2016 MobileTouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
